33768652ee7af8382cc37dcf1f5ded55ce923104
unknown <chinecheremmbah64@gmail.com>
2022-07-12 09:38:51 -0700

    Updated settings.py

--- mango/settings.py
+++ mango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-*^5s566d(7i5sz&ei^idi+y#6&mg!=sm2i*r!r-j#@13cl=9j7'
+SECRET_KEY = 'django-insecure-*^5s566d(7i5sz&ei^idi+y#6&mg!=sm2i*r!r-j#@13cl=9j7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
