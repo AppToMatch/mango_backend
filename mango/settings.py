5d11caddc7b85e1a7184c8e4f952e8ca7dbb9489
artificodes <chinecheremmbah64@gmail.com>
2026-02-11 08:35:39 +0100

    Env changes

--- mango/settings.py
+++ mango/settings.py
@@ -148,9 +148,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
-django_heroku.settings(locals())
-try:
-    from .local_settings import *
-except ImportError:
-    pass
